bd446f1534856c7cb3e86ce4269c73b411971ec6
Jean-Sebastien Jeannotte <jjeannotte@Jean-Sebastiens-MacBook-Pro-2.local>
2013-10-29 00:07:55 -0700

    Move the Sublime files config back in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,18 @@ function doIt() {
 		--exclude "Sublime Text 2"  \
 		--exclude "root"  \
 		-av --no-perms . ~
+	if [ -d ~/Library ]; then
+	    if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old ]; then
+	        mv ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages-old
+	        mv ~/Library/Application\ Support/Sublime\ Text\ 2/Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Packages-old
+	        mv ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages-old
+	        ln -sf `pwd`/Sublime\ Text\ 2/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages
+	        ln -sf `pwd`/Sublime\ Text\ 2/Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+	        ln -sf `pwd`/Sublime\ Text\ 2/Pristine\ Packages ~/Library/Application\ Support/Sublime\ Text\ 2/Pristine\ Packages
+	    else
+	        echo "Skipping Sublime Symlink creation for config (normaly happens when the bootrap script was already run)."
+	    fi
+	fi
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
